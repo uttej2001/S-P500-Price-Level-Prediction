6ed0094199e4801ba33e84f9b4dcdcaf1311d9e7
UTTEJKUMAR <72940291+uttej2001@users.noreply.github.com>
2021-09-27 10:22:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"k.uttejkumar@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
